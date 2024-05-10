0a360a832b119dee3799eb07c56e38732b8746cd
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-05-10 11:04:01 -0500

    Add: Base UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='flex flex-col min-h-screen'>
+      <nav className='h-12 bg-slate-300'>NAV</nav>
+      <main className='flex flex-row flex-1'>
+        <aside className='bg-slate-200 w-60'>SIDEBAR</aside>
+        <section className='flex-1 bg-slate-100'>WORKSPACE</section>
+      </main>
+    </div>
   );
 }
 
