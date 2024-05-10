0d05f85d3ccb2d7fcee62bd6041f650b4bac7f8f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-05-10 11:10:45 -0500

    Add: Background color and border colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <nav className='h-12 bg-slate-300'>NAV</nav>
+    <div className='flex flex-col text-white min-h-screen'>
+      <nav className='h-12 bg-[#1D2126] border-b border-[#2C323B]'>NAV</nav>
+
       <main className='flex flex-row flex-1'>
-        <aside className='bg-slate-200 w-60'>SIDEBAR</aside>
-        <section className='flex-1 bg-slate-100'>WORKSPACE</section>
+        <aside className='bg-[#191B23] w-60 border-r border-[#2C323B]'>
+          <nav className='h-14 border-b bg-[#191B23] border-[#2C323B]'>
+            Sidebar Nab
+          </nav>
+        </aside>
+
+        <section className='flex-1 bg-slate-200 text-black'>WORKSPACE</section>
       </main>
     </div>
   );
