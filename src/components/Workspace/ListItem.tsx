c2366e7a351b74dd160708d85bfc0d5857adafd8
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-05-27 18:10:18 -0500

    Add: Ability to add items into boards

--- src/components/Workspace/ListItem.tsx
+++ src/components/Workspace/ListItem.tsx
@@ -1,11 +1,25 @@
 import { CirclePlus, MoreHorizontal } from 'lucide-react';
 import { type IList } from '../../types/board';
+import { useState } from 'react';
 
 type ListItemProps = {
   list: IList;
 };
 
 const ListItem = ({ list }: ListItemProps) => {
+  const [listItems, setListItems] = useState<string[]>(list.items);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setListItems((prev) => [...prev, input]);
+
+    setInput('');
+    setIsEditing(false);
+  };
+
   return (
     <li className='w-64 p-2 bg-[#0F1203] rounded-md h-fit'>
       <header className='flex flex-row justify-between'>
@@ -15,7 +29,7 @@ const ListItem = ({ list }: ListItemProps) => {
 
       {/* LIST ITEMS STARTS */}
       <ul className='my-2 space-y-2'>
-        {list.items.map((item, idx) => (
+        {listItems.map((item, idx) => (
           <li className='bg-[#22272C] p-1 w-full rounded-md' key={idx}>
             <span className='ml-2'>{item}</span>
           </li>
@@ -24,7 +38,21 @@ const ListItem = ({ list }: ListItemProps) => {
       {/* LIST ITEMS ENDS */}
 
       <footer>
-        <button className='flex flex-row items-center gap-2 hover:bg-[#22272C] p-1 w-full rounded-md'>
+        {isEditing ? (
+          <form className='bg-[#22272C] p-1 rounded' onSubmit={handleSubmit}>
+            <input
+              className='bg-transparent outline-none'
+              type='text'
+              placeholder='Some Text'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </form>
+        ) : null}
+        <button
+          className='flex flex-row items-center gap-2 hover:bg-[#22272C] p-1 w-full rounded-md'
+          onClick={() => setIsEditing(true)}
+        >
           <CirclePlus className='ml-2 w-4 h-4' />
           Add Card
         </button>
